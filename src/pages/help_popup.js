7c5920fa3e35239bfc7b4db5b169c21710d9f2dd
Vivian Conrad <44071734+vivianconrad@users.noreply.github.com>
2022-10-05 12:57:41 -0500

    add pages

---
+++ src/pages/help_popup.js
@@ -0,0 +1,9 @@
+//will be a module
+
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import '../css/index.css';
+import App from '../App';
+import * as serviceWorkerRegistration from '../serviceWorkerRegistration';
+import reportWebVitals from '../reportWebVitals';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import '../css/index.css';
+import App from '../App';
+import * as serviceWorkerRegistration from '../serviceWorkerRegistration';
+import reportWebVitals from '../reportWebVitals';
 
 const percentage = 66;
 const DATA = [
